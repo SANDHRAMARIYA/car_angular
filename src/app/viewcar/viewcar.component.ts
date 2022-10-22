a3da43a84bff0cd4ec2854a7029f1def5ea63124
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-22 23:29:55 +0530

    view

---
+++ src/app/viewcar/viewcar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcar',
+  templateUrl: './viewcar.component.html',
+  styleUrls: ['./viewcar.component.css']
+})
+export class ViewcarComponent implements OnInit {
+
+  constructor() { }
+
+
+
+
+  carData:any=[]
+  ngOnInit(): void {
+  }
+
+}
