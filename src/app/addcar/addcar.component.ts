1599b826e896bebdb5e699da51ef99cb85660b99
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-23 18:34:47 +0530

    servve

--- src/app/addcar/addcar.component.ts
+++ src/app/addcar/addcar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcar',
@@ -7,7 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
+
+
+carname=""
+carcolor=""
+carbrand=""
+cardescription=""
+carprice=""
+status:boolean=false
+
+readValues=()=>{
+  let data={
+    "carname":this.carname,
+    "carcolor":this.carcolor,
+    "carbrand":this.carbrand,
+    "cardescription":this.cardescription,
+    "carprice":this.carprice
+  }
+console.log(data)
+this.myapi.addCar(data).subscribe(
+  (response)=>{
+  console.log(response)
+  alert("success")
+  this.carname=""
+  this.carcolor=""
+  this.carbrand=""
+  this.cardescription=""
+  this.carprice=""
+  this.status=true
+  }
+)
+  }
+  
+
 
   ngOnInit(): void {
   }
--- src/app/viewcar/viewcar.component.ts
+++ src/app/viewcar/viewcar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewcar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewcarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) {
+    this.fetchData()
+   }
 
-
+fetchData=()=>{
+   this.myapi.viewCar().subscribe(
+    (data)=>{
+      this.carData=data
+    }
+  )
+}
 
 
   carData:any=[]
